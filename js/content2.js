6bf926c971112f997fda42301d0b876a85d21f5d
stubtail42 <jqugali@gmail.com>
2020-11-11 17:45:57 -0800

    js files

---
+++ js/content2.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let favColor = prompt('What is you favorite color Panda?');
+let color;
+
+if(favColor != 'red'){
+    color = 'Boooo';
+} else if (favColor = 'red'){
+    color = 'Yeaaaaa'
+} else color = 'whaaaaaaa'
+
+document.write('<h4>' + color + '</h4>' )
