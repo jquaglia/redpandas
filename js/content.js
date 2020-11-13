35bfaae028bff82c5673bf3c05e240ed661a264d
stubtail42 <jqugali@gmail.com>
2020-11-12 17:55:55 -0800

    finally figured out how to get the image to repeat

--- js/content.js
+++ js/content.js
@@ -18,22 +18,48 @@ function dailyGreeting(){
     document.write('<h3>' + greeting + '</h3>');
 }
 
+// function colorPanda(){
+//     let favColor = prompt('What is you favorite color Panda?');
+//     let color;
+//     let goodColor = ['red', 'black']
+
+//  if(favColor != 'red'){
+//     color = 'Boooo';
+// } else if (favColor = 'red'){
+//     color = 'Yeaaaaa'
+// } else color = 'Whaaaaaa'
+
+
+
+// document.write('<h4>' + color + '</h4>' )
+// }
+// //
+// //
+
+
 function colorPanda(){
-    let favColor = prompt('What is you favorite color Panda?');
-    let color;
-    let goodColor = ['red', 'black']
+// let correctAnswer = 'black';
+// let correctAnswer1 = 'red';
+let color;
 
- if(favColor != 'red'){
-    color = 'Boooo';
-} else if (favColor = 'red'){
-    color = 'Yeaaaaa'
-} else color = 'Whaaaaaa'
-
-
-
-document.write('<h4>' + color + '</h4>' )
+let favColor = prompt('What is your favorite color Panda? (red or black)');
+while(favColor != 'red'){
+    if(favColor === 'black'){
+        break;
+    }
+    favColor = prompt('Try again! (red or black)');
+    }
+    if(favColor === 'black'){
+        color = 'Boooo';
+    } else if(favColor === 'red'){
+        color = 'Yeaaaaaa'
 }
 
+document.write('<h4>' + color + '</h4>')
+}
+
+//
+//
 function clickButton(){
     let message;
     let x = confirm('click ok');
@@ -65,3 +91,34 @@ function twentyTwenty(){
 // dailyGreeting(); 
 // colorPanda(); 
 // twentyTwenty();
+
+function rateThis(){
+    let userAnswer = prompt('Rate Red Pandas on a scale of 1 to 5');
+    if(userAnswer > 0 && userAnswer <= 5){
+        for(let i = 0; i < userAnswer; i++){
+            document.write('<img src="https://scx1.b-cdn.net/csz/news/800/2020/redpandasmay.jpg">');
+        }
+    } else{
+        alert('Invalid answer')
+    }
+}
+
+rateThis();
+
+// function rateThis(){
+//     let userAnswer = prompt('Rate Red Pandas on a scale of 1 to 5');
+//     if(userAnswer > 0 && userAnswer <= 5){
+//         enjoyPhoto();
+//     } else {
+//         alert('Invalid Answer')
+//     }
+// }
+
+// function enjoyPhoto(){
+//     for (let i = 0; i < userAnswer; i++){
+//         document.write('<img src="https://scx1.b-cdn.net/csz/news/800/2020/redpandasmay.jpg">');
+//     }
+// }
+
+// rateThis();
+   
